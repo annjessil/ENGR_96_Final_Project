a070a2deb164b58940c27f7c40b1b62688c33d67
annjessil <92198450+annjessil@users.noreply.github.com>
2024-02-13 20:09:10 -0800

    Moved tiles to tile folder, created functions in player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+
+    Rigidbody2D rb;
+
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Vector3 norm = collision.GetContact(0).normal;
+        if (Vector3.Angle(norm, Vector3.up) < 45f)
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
